dd5747eea79809aaa9b020b4b09ae4ccbc729569
Merilin Ali <merilinayhan@gmail.com>
2020-12-04 14:56:08 +0300

    FotoYukleme
    Upload Photos in JPG, PNG, GIF type

---
+++ FotografYukleme/FotografYukleme/Controllers/YuklemeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace FotografYukleme.Controllers
+{
+    public class YuklemeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("FotografYukleme")]
+        public async Task<IActionResult> Index(List<IFormFile> files)
+        {
+			long size = files.Sum(f => f.Length);
+
+			var filePaths = new List<string>();		
+			foreach (var formFile in files)
+			{
+				if (formFile.Length > 0)
+				{
+					var filePath = Path.GetTempFileName();
+					filePaths.Add(filePath);
+
+					using (var stream = new FileStream(filePath, FileMode.Create))
+					{
+						await formFile.CopyToAsync(stream);
+					}
+				}
+			}
+
+			return Ok(new { count = files.Count, size, filePaths });
+		}
+    }
+}
